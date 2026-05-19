b681a7287245d5353cce9a5c6d8dd782918b430a
Dikshya Gouli <dikshyagouli1@gmail>
2026-05-19 13:01:03 +0545

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-red-600'>css test</h1>
+      <Button>Rocket</Button>
+    </div>
+  )
+}
